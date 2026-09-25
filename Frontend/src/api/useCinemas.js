e99dbc7be5f66f35301e84e01cf54b90aca5ed5e
tiendat <3123560014@sv.sgu.edu.vn>
2026-09-25 08:43:27 +0700

    add2

--- Frontend/src/api/useCinemas.js
+++ Frontend/src/api/useCinemas.js
@@ -1,12 +1,35 @@
 import axiosInstance from './axiosInstance';
 
 /**
- * API Module: Cinemas, Auditoriums & Physical Seats (Phụ trách: Dev B)
- * Quản lý toàn bộ API Cụm rạp, Phòng chiếu, Loại ghế và Sơ đồ ghế mẫu
+ * API Module: Cinemas, Auditoriums & Physical Seats
+ * Phụ trách: Dev B
+ *
+ * Quản lý:
+ * - Danh sách cụm rạp
+ * - Chi tiết cụm rạp
+ * - Danh sách ghế của phòng chiếu
  */
 
 export const useCinemas = {
 
+    // Lấy tất cả cụm rạp, có thể lọc theo thành phố
+    getAllCinemas: async (city = '') => {
+        return await axiosInstance.get('/cinemas', {
+            params: city ? { city } : {}
+        });
+    },
+
+    // Lấy thông tin chi tiết một cụm rạp
+    getCinemaById: async (id) => {
+        return await axiosInstance.get(`/cinemas/${id}`);
+    },
+
+    // Lấy danh sách ghế của một phòng chiếu
+    getAuditoriumSeats: async (auditoriumId) => {
+        return await axiosInstance.get(
+            `/auditoriums/${auditoriumId}/seats`
+        );
+    }
 };
 
 export default useCinemas;
